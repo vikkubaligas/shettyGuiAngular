d98f6214e66f6cfa39d30b3ed1f7272731ea6cf6
Vikram Baliga <vikkubaligatmp@gmail.com>
2023-10-21 22:01:14 +0000

    app.js and form.js deleted after full working

---
+++ axen/node_app/config.js
@@ -0,0 +1,11 @@
+
+var MID = "VqSJZP66509836763782";
+var MKEY = "0RkFzm%V9PGG9yGo";
+
+var ENV= 'securegw.paytm.in';
+var WEBSITE= 'DEFAULT';
+
+exports.MID = MID;
+exports.MKEY = MKEY;
+exports.ENV = ENV;
+exports.WEBSITE = WEBSITE;
